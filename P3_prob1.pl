7213885f64139e56b026f80ccebaf7f6f0165d99
915-Negrila-Iulia <72079446+915-Negrila-Iulia@users.noreply.github.com>
2022-02-20 00:54:40 +0200

    Update P3_prob1.pl

--- P3_prob1.pl
+++ P3_prob1.pl
@@ -1,5 +1,6 @@
-%Write a predicate to generate the list of all subsets
-%with all elements of a given list
+% 1.
+% Write a predicate to generate the list of all subsets with all elements of a given list. 
+% Eg: [2, 3, 4] N=2 => [[2,3],[2,4],[3,4]]
 
 %combinations
 %comb(L-list,K-integer,R)
