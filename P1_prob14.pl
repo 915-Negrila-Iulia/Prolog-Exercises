5e0735d05e52c083a33c8e4aee6c93128a0d87ab
915-Negrila-Iulia <72079446+915-Negrila-Iulia@users.noreply.github.com>
2022-02-20 00:51:14 +0200

    Update P1_prob14.pl

--- P1_prob14.pl
+++ P1_prob14.pl
@@ -1,3 +1,7 @@
+%14.
+%a. Write a predicate to test equality of two sets without using the set difference.
+%b. Write a predicate to select the n-th element of a given list.
+
 % a %
 
 %existence of a number in a set
